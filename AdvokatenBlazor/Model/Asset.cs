65f8011a9cdaa785877da5afa5f9d4be149b8d9a
Jonathan Lipinski Rasmussen <jonathanlipinskirasmussen@gmail.com>
2024-11-15 13:27:48 +0100

    Krav2

--- AdvokatenBlazor/Model/Asset.cs
+++ AdvokatenBlazor/Model/Asset.cs
@@ -1,10 +1,43 @@
-﻿namespace AdvokatenBlazor.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdvokatenBlazor.Model
 {
+    public enum AssetType
+    {
+        Property,
+        Vehicle,
+        Item,
+        Stock,
+        Money,
+        None
+    }
+
     public abstract class Asset
     {
-        public string Name { get; set; }
         public double Value { get; set; }
-        public double PercentageOwned { get; set; }
+        private int _percentageOwned;
+
+        public int PercentageOwned
+        {
+            get { return _percentageOwned; }
+            set {
+
+                if (value > 100)
+                {
+                    _percentageOwned = 100;
+                }
+                else if (value < 0)
+                {
+                    _percentageOwned = 0;
+                }
+                else
+                {
+                    _percentageOwned = value;
+                }
+            }
+        }
+
+        public AssetType AssetType { get; set; }
     }
 
     public class Property : Asset
