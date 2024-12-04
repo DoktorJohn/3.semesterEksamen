8f2f17a636d3219c292868f9e574b8bf5d4b00fd
Jonathan Lipinski Rasmussen <jonathanlipinskirasmussen@gmail.com>
2024-12-04 22:53:43 +0100

    yes

--- AdvokatenBlazor/obj/Debug/net8.0/AdvokatenBlazor.AssemblyInfo.cs
+++ AdvokatenBlazor/obj/Debug/net8.0/AdvokatenBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvokatenBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fafcad8fd9ce00c026d7bfabfe6276013932995")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e8d25de5cd9ddb107765ae1eb221ca20183bcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvokatenBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvokatenBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
